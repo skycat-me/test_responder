1496feede352f1117136ff6b15c0a00ef9cc2f26
Skycat <mt@skycat.me>
2019-08-07 22:41:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# test_responder
+Responderのお試しテスト
---
+++ test.py
@@ -0,0 +1,11 @@
+import responder
+
+api = responder.API()
+
+@api.route("/")
+def hello_world(req, resp):
+    resp.text = "hello, world!"
+
+if __name__ == '__main__':
+    api.run()
+
